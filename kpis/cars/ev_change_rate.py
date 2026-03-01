d9b4c2ea9d718397d89a2359ff5df7b0f6aadbe9
elvbom <ecdb@protonmail.com>
2026-03-01 08:16:49 +0100

    add test and break out old ev calc

--- kpis/cars/ev_change_rate.py
+++ kpis/cars/ev_change_rate.py
@@ -6,8 +6,9 @@ import pandas as pd
 PATH_CARS_DATA = "kpis/cars/sources/kpi1_calculations.xlsx"
 
 
-def get_ev_change_rate(df, to_percent: bool = True):
-    """Calculate the change rate of newly registered rechargeable cars per municipality and year."""
+def get_ev_change_rate_2015_to_2024(to_percent: bool = True):
+    """Get change rate of newly registered rechargeable cars per municipality and year 2015-2024.
+    This is an old data source, that we used before we had all calculations in this file."""
     df_raw_cars = pd.read_excel(PATH_CARS_DATA)
 
     df_raw_cars.columns = df_raw_cars.iloc[1]  # name columns after row
@@ -25,18 +26,23 @@ def get_ev_change_rate(df, to_percent: bool = True):
         2022,
         2023,
         2024,
-    ]  # NOTE: this needs to be updated if the data is updated
+    ]
 
     for year in years:
         df_raw_cars[f"evChange_{year}"] = df_raw_cars[year] * (100 if to_percent else 1)
 
-    df_raw_cars["evChangeRate"] = df_raw_cars["evChangeRate"] * (
+    df_raw_cars["evChangeRate"] = pd.to_numeric(df_raw_cars["evChangeRate"], errors='coerce') * (
         100 if to_percent else 1
     )
 
     yearly_columns = [f"evChange_{year}" for year in years]
     df_cars = df_raw_cars.filter(["Kommun", "evChangeRate"] + yearly_columns, axis=1)
+    return df_cars
 
+def get_ev_change_rate(df, to_percent: bool = True):
+    """Calculate the change rate of newly registered rechargeable cars per municipality and year."""
+
+    df_cars = get_ev_change_rate_2015_to_2024(to_percent)
     df = df.merge(df_cars, on="Kommun", how="left")
 
     return df
--- tests/test_ev_change_rate.py
+++ tests/test_ev_change_rate.py
@@ -2,11 +2,32 @@
 import unittest
 import pandas as pd
 
-from kpis.cars.ev_change_rate import get_ev_change_rate
+from kpis.cars.ev_change_rate import get_ev_change_rate, get_ev_change_rate_2015_to_2024
 
 
 class TestBicycleCalculations(unittest.TestCase):
     """Test the ev change rate calculations"""
+
+    def test_get_ev_change_rate_2015_to_2024(self):
+        """Test the get_ev_change_rate_2015_to_2024 function"""
+        df_expected = pd.DataFrame(
+            {
+                "Kommun": ["Ale"],
+                "evChangeRate": [7.22114538969724],
+                "evChange_2015": [2.64385692068429],
+                "evChange_2016": [1.28865979381443],
+                "evChange_2017": [1.64383561643836],
+                "evChange_2018": [5.02873563218391],
+                "evChange_2019": [7.08860759493671],
+                "evChange_2020": [27.338129496402903],
+                "evChange_2021": [41.7276720351391],
+                "evChange_2022": [55.6171983356449],
+                "evChange_2023": [54.9056603773585],
+                "evChange_2024": [48.8612836438923],
+            }
+        )
+        df_result = get_ev_change_rate_2015_to_2024(to_percent=True)
+        pd.testing.assert_frame_equal(df_result.iloc[:1], df_expected)
 
     def test_get_ev_change_rate(self):
         """Test the get_ev_change_rate function"""
